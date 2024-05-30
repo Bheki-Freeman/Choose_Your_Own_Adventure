ab7e416cfe36c7cc72f401c77642024458cd85d8
Bheki-Freeman <bhekisisadlamini05@gmail.com>
2024-05-30 13:45:06 +0200

    Defined the start-game mechanism, modified player login & register mechanisms

--- main.py
+++ main.py
@@ -8,42 +8,95 @@ today = date.today()
 player = Player()
 
 # the login from the main class(it invokes the player login)
+
+
 def login():
-	if player.login():
-		start_game()
-	else:
-		print('Login Failure') # if the player failed to login, let  him to register
-		register = input('New to the program? [yes / no]: ').lower()
-		if register == 'yes':
-			player.register()
-		else:
-			login()
+    login_status, login_user_name = player.login()
+    if login_status:
+        time.sleep(1)
+        player.fetch_data(login_user_name)
+        start_game()
+    else:
+        # if the player failed to login, let  him to register
+        time.sleep(1)
+        print(f'{line}\n\t--- [ACCESS DENIED!] ---\n{line}\n')
+        register = input('New to the program? [yes / no]: ').lower()
+        if register == 'yes':
+            registar()
+        else:
+            login()
+
+
+def registar() -> None:
+    if player.register():
+        print('[REGISTRATION SUCCESSFUL]')
+        login()
+    else:
+        print('[REGISTRATION ABORTED!]')
+
 
 def start_game():
-	print(f'Now Starting the Game')
-	quit_game()
+    print(f'{line}\n\t ---- [GAME STARTED] -----\n{line}\n')
+    print(f'>>GREETINGS {player.first_name} Where Do we Start?')
+    start = int(input(
+        '\t1 ---- Go Town\n\t2 ---- Stay Home\n\t3 ---- Go to the Farm\n\t4 ---- Feeling In Love\n\t0 ---- Menu\n\t[CHOICE]: '))
+    if start == 1:
+        go_town()
+    elif start == 2:
+        stay_home()
+    elif start == 3:
+        go_to_farm()
+    elif start == 4:
+        feeling_in_love()
+    elif start == 0:
+        game_over_screen()
+        quit()
+    else:
+        print('[ERROR]: Wrong user input!')
+        start_game()
+
+
+def go_town():
+    print('Going to town')
+
+
+def stay_home():
+    print('Staying Home')
+
+
+def go_to_farm():
+    print('Going to the Farm')
+
+
+def feeling_in_love():
+    print('Do you really have a Lover?')
+
+
 def quit_game():
-	quit_game = input('Do you wanna quit? [yes / no]: ').lower()
-	if quit_game == 'yes':
-		game_over_screen()
-	else:
-		welcome_player()
+    quit_game = input('Do you wanna quit? [yes / no]: ').lower()
+    if quit_game == 'yes':
+        game_over_screen()
+    else:
+        welcome_player()
+
 
 def game_over_screen() -> None:
-	print('-- Game Over --')
-	print('Your Scores: ')
+    print('-- Game Over --')
+    print('Your Scores: ')
+
 
 def welcome_player() -> None:
-	print(f'{line}\n\t--- Welcome Player ---\n{line}\nHope you enjoy playing the game ahead!\n')
-	print(f'{line}\n--- [DATE] {today} --- [TIME] {cur_time.hour}:{cur_time.minute}:{cur_time.second} ---\n{line}\n')
-	register = input('New to the program? [yes / no]: ').lower()
-	if register == 'yes':
-		if player.register():
-			login()
-	elif register == 'no':
-		login()
-	else:
-		print('Wrong User input')
-		welcome_player()
+    print(f'{line}\n\t--- Welcome Player ---\n{line}\nHope you enjoy playing the game ahead!\n')
+    print(
+        f'{line}\n--- [DATE] {today} --- [TIME] {cur_time.hour}:{cur_time.minute}:{cur_time.second} ---\n{line}\n')
+    register = input('New to the program? [yes / no]: ').lower()
+    if register == 'yes':
+        registar()
+    elif register == 'no':
+        login()
+    else:
+        print('Wrong User input')
+        welcome_player()
+
 
 welcome_player()
--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+from getpass import getpass
 import sqlite3
 
 conn = sqlite3.connect('main.db')
@@ -7,6 +8,16 @@ user_name = None
 
 
 class Player():
+    def fetch_data(self, username) -> None:
+        sql = f'SELECT first_name, last_name, user_name, phone_number, age_range FROM player WHERE user_name="{username}"'
+        result = cur.execute(sql).fetchall()
+        if len(result) == 0:
+            print('[PLAYER NOT FOUND!]')
+        else:
+            for row in result:
+                first_name, last_name, user_name, phone_number, age_range = row
+                self.set_values(first_name, last_name, user_name, phone_number, age_range)
+                
     def set_values(self, first_name, last_name, user_name, phone_number, age_range) -> None:
         self.first_name = first_name
         self.last_name = last_name
@@ -16,13 +27,11 @@ class Player():
 
     def create_username(self) -> str:
         user_name = input('[USERNAME]: ')
-
         def check_username(username) -> str:
             global user_name
             sql = f'SELECT user_name from player WHERE user_name="{username}"'
             result = cur.execute(sql).fetchall()
             if not result:
-                print('username free')
                 user_name = username
             else:
                 logon = input('[USERNAME TAKEN] Login? yes / no: ')
@@ -34,7 +43,6 @@ class Player():
         return check_username(user_name)
 
     def register(self) -> bool:
-        print(f'Registering Now')
         first_name = input('[FIRST NAME]: ')
         last_name = input('[LAST NAME]: ')
         user_name = self.create_username()
@@ -44,16 +52,26 @@ class Player():
                 '[AGE RANGE]\n\t1 --- (-10)\n\t2 --- (10 -> 20)\n\t3 --- (21 -> 30)\n\t4 --- (31 -> 40)\n\t5 --- (40+)\n\t[CHOICE]: ')
             self.set_values(first_name, last_name, user_name,
                             phone_number, age_range)
-            sql = '''INSERT INTO player(first_name, last_name, user_name, phone_number, age_range) VALUES(?, ?, ?, ?, ?)'''
-            print(self.user_name)
+            password = getpass('[PASSWORD]: ')
+            sql = '''INSERT INTO player(first_name, last_name, user_name, phone_number, age_range, password) VALUES(?, ?, ?, ?, ?, ?)'''
             data = (self.first_name, self.last_name, self.user_name,
-                    self.phone_number, self.age_range)
+                    self.phone_number, self.age_range, password)
             cur.execute(sql, data)
             conn.commit()
             return True
         else:
             return False
 
-    def login(self) -> bool:
-        print(f'Login Now')
-        return True
+    def login(self) -> dict[bool, str]:
+        login = False
+        self.user_name = None
+        user_name = input('[USERNAME]: ')
+        password = getpass('[PASSWORD]: ')
+        sql = f'SELECT user_name, password FROM player WHERE user_name="{user_name}" AND password="{password}"'
+        result = cur.execute(sql).fetchall()
+        if not result:
+            login = False
+        else:
+            self.user_name = user_name
+            login = True        
+        return {login, self.user_name} # I will need to use the user_name as the program runs
